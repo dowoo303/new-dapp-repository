6937386a7fb56e35c130ca9f90291f6894fff7dc
dowoo303 <dowoo303@naver.com>
2023-03-30 16:44:49 +0900

    DAY11 last

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,73 @@ function App() {
           </ul>
         </div>
       </header>
+      <main>
+        {/* Introduce */}
+        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          내 소개 적는 곳
+        </div>
+        <ul className="bg-indigo-100 max-w-screen-xl m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-8 py-8">
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* {object-fit:cover} */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* {object-fit:cover} */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* {object-fit:cover} */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* {object-fit:cover} */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* {object-fit:cover} */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* {object-fit:cover} */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
+        <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          내 연락처
+        </div>
+      </main>
+      <footer className="bg-slate-400 max-w-screen-xl mx-auto flex justify-end py-1 md:py-2 pr-2">
+        2023, Designed by, dongwoopark
+      </footer>
     </div>
   );
 }
